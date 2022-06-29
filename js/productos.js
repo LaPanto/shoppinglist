e491fe9ffef59eadd8f2c705048111d970a33850
dori <doribepa@gmail.com>
2022-06-29 10:38:13 +0200

    añadido productos en js

---
+++ js/productos.js
@@ -0,0 +1,25 @@
+let col_productos;
+
+/*
+col_productos={
+    productos:[
+        {nombre: "cerveza",
+         cantidad: 24,
+         tienda: "Alcampo",
+         notas: "Las de San Miguel están de oferta"},
+
+        {nombre: "arroz",
+         cantidad: 2,
+         tienda: "Mercadona",
+         notas: "Tipo Bomba"},     
+    ]
+}
+*/
+function creaProducto() {
+    let nombreProducto = document.getElementById("inputNombre").value;
+    let cantidadProducto= document.getElementById("inputCantidad").value;
+    let tiendaProducto= document.getElementById("tiendas").value;
+    let notasProducto= document.getElementById("notasProducto").value;
+    
+    
+}
--- js/tiendas.js
+++ js/tiendas.js
@@ -1,49 +1,38 @@
-function selecTienda() {
-    let select = document.getElementById("tiendas");
-    let opciones = "";
-    opciones = `<option value="no seleccionado">  </option>`;
-    for (tienda of col_tiendas.tiendas) {
-        opciones += `<option value= ${tienda.nombre} > ${tienda.nombre} </option>`
-    }
-    select.innerHTML = opciones;
-}
 
 let col_tiendas; /*coleccion de tiendas*/
 
 col_tiendas = {
-    tiendas: [{
-            nombre: "Mercadona"
-        },
-        {
-            nombre: "FamilyCash"
-        },
-        {
-            nombre: "Consum"
-        },
-        {
-            nombre: "Aldi"
-        },
-        {
-            nombre: "MaxiDia"
-        },
-        {
-            nombre: "Lidl"
-        },
-        {
-            nombre: "Alcampo"
-        },
-        {
-            nombre: "Maximas"
-        },
-        {
-            nombre: "CashFamily"
-        },
-        {
-            nombre: "Carrefour"
-        },
-        {
-            nombre: "Mercado"
-        },
-
+    tiendas: [
+        {nombre: "Mercadona"},
+        {nombre: "FamilyCash"},
+        {nombre: "Consum"},
+        {nombre: "Aldi"},
+        {nombre: "MaxiDia"},
+        {nombre: "Lidl"},
+        {nombre: "Alcampo"},
+        {nombre: "Maximas"},
+        {nombre: "CashFamily"},
+        {nombre: "Carrefour"},
+        {nombre: "Mercado"}
     ]
+}
+
+
+/**
+ * Sirve para rellenar una seleccion de un input
+ * @param {*} idSelect Es el id del select que rellenamos
+ * @param {*} coleccion Es la coleccion de JSON de las que creamos nuestras tiendas
+ */
+function rellenaSelect(idSelect, coleccion) {
+
+    let select = document.getElementById(idSelect);
+    let opciones = "";
+
+    opciones = `<option value="no seleccionado">  </option>`;
+
+    for (tienda of coleccion) {
+        opciones += `<option value= ${tienda.nombre} > ${tienda.nombre} </option>`
+    }
+
+    select.innerHTML = opciones;
 }
